7d6262ccf1275327ee004d841166ff4846989f78
VincenzoDeLucia <vincenzo.delucia@icloud.com>
2020-12-14 18:41:46 +0100

    Figured out the API response

--- src/pages/Moviedisplay.jsx
+++ src/pages/Moviedisplay.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import io from "socket.io-client";
 
 class Moviedisplay extends Component {
   state = {
@@ -6,7 +7,13 @@ class Moviedisplay extends Component {
     score: [],
   };
 
-  componentDidMount() {}
+  componentDidMount() {
+    // const socket = io("ws://localhost:5005");
+    // //socket.on('connection', )
+    // socket.on("message", (response) => {
+    //   console.log(`${response}`);
+    // });
+  }
 
   //Steps in rendering of the movies
   //Load the array incl images
@@ -14,7 +21,7 @@ class Moviedisplay extends Component {
 
   //The right button both increases score property by 1 and goes the next element of the loop
   handleRightButton() {
-    this.setState;
+    //this.setState;
 
     return;
   }
@@ -28,6 +35,7 @@ class Moviedisplay extends Component {
     return (
       <div>
         <h2>The movies are supposed to be displayed here</h2>
+
         <button onClick={() => this.handleRightButton()}>Dislike </button>
         <button onClick={() => this.handleRightButton()}>Like</button>
       </div>
